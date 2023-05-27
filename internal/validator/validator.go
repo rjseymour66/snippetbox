8446c8f0c06e113d3c5be86e2a22e62fa0dc01bf
rseymour <rjs.github@gmail.com>
2023-05-27 10:32:21 -0400

    completed up to User authorization

--- internal/validator/validator.go
+++ internal/validator/validator.go
@@ -1,22 +1,35 @@
 package validator
 
 import (
+	"regexp"
 	"strings"
 	"unicode/utf8"
 )
 
-// Define a new Validator type which contains a map of validation errors for our
+// EmailRX returns a pointer to a 'compiled' regexp.Regexp type, or panics
+// in the event of an error.is parsed and compiled once at startup.
+// This
+var EmailRX = regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+\\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
+
+// Validator type which contains a map of validation errors for our
 // form fields.
 type Validator struct {
-	FieldErrors map[string]string
+	NonFieldErrors []string
+	FieldErrors    map[string]string
 }
 
-// Valid() returns true if the FieldErrors map doesn't contain any entries.
+// Valid returns true if the FieldErrors map doesn't contain any entries.
 func (v *Validator) Valid() bool {
-	return len(v.FieldErrors) == 0
+	return len(v.FieldErrors) == 0 && len(v.NonFieldErrors) == 0
 }
 
-// AddFieldError() adds an error message to the FieldErrors map (so long as no
+// AddNonFieldError adds validation errors that are not related to a specific
+// form field.
+func (v *Validator) AddNonFieldError(message string) {
+	v.NonFieldErrors = append(v.NonFieldErrors, message)
+}
+
+// AddFieldError adds an error message to the FieldErrors map (so long as no
 // entry already exists for the given key).
 func (v *Validator) AddFieldError(key, message string) {
 	// Note: We need to initialize the map first, if it isn't already
@@ -30,7 +43,7 @@ func (v *Validator) AddFieldError(key, message string) {
 	}
 }
 
-// CheckField() adds an error message to the FieldErrors map only if a
+// CheckField adds an error message to the FieldErrors map only if a
 // validation check is not 'ok'.
 func (v *Validator) CheckField(ok bool, key, message string) {
 	if !ok {
@@ -38,17 +51,17 @@ func (v *Validator) CheckField(ok bool, key, message string) {
 	}
 }
 
-// NotBlank() returns true if a value is not an empty string.
+// NotBlank returns true if a value is not an empty string.
 func NotBlank(value string) bool {
 	return strings.TrimSpace(value) != ""
 }
 
-// MaxChars() returns true if a value contains no more than n characters.
+// MaxChars returns true if a value contains no more than n characters.
 func MaxChars(value string, n int) bool {
 	return utf8.RuneCountInString(value) <= n
 }
 
-// PermittedInt() returns true if a value is in a list of permitted integers.
+// PermittedInt returns true if a value is in a list of permitted integers.
 func PermittedInt(value int, permittedValues ...int) bool {
 	for i := range permittedValues {
 		if value == permittedValues[i] {
@@ -56,4 +69,15 @@ func PermittedInt(value int, permittedValues ...int) bool {
 		}
 	}
 	return false
+}
+
+// MinChars returns true if a value contains at least n characters.
+func MinChars(value string, n int) bool {
+	return utf8.RuneCountInString(value) >= n
+}
+
+// Matches returns true if a value matches a provided compiled regular
+// expression pattern.
+func Matches(value string, rx *regexp.Regexp) bool {
+	return rx.MatchString(value)
 }
